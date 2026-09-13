8f1434abd985d5b2913de5a77a8c812383b0067b
Muhammed Saeed <saeed.anyit@gmail.com>
2026-09-13 13:01:57 +0500

    problem fixed

--- downloader/service/instagram.py
+++ downloader/service/instagram.py
@@ -19,13 +19,10 @@ progress={}
 def playwright_worker():
     print("playwright working is start....")
     with sync_playwright()as p:
-        mobile = p.devices["iPhone 13"].copy()
-        mobile.pop("default_browser_type", None)
 
         context=p.chromium.launch_persistent_context(
         user_data_dir="/app/playwright_profile",
         headless=True,
-        **mobile
        )
         print("playwright broswer is lanuch and wait your task")
 
@@ -71,18 +68,18 @@ def playwright_worker():
             print("TOUCH:", page.evaluate("navigator.maxTouchPoints"))
             print("WIDTH:", page.evaluate("window.innerWidth"))          
 
-            # page.route(
-            #      "**/*",
-            #     lambda route: (
-            #          route.abort()
-            #          if(
-            #                 route.request.resource_type in [ "stylesheet", "font","image"]
-            #                 # or "api/graphql" in route.request.url
-            #                 # or "/ajax/bz" in route.request.url
-            #          )
-            #          else route.continue_()
-            #        )
-            #     )  
+            page.route(
+                 "**/*",
+                lambda route: (
+                     route.abort()
+                     if(
+                            route.request.resource_type in [ "stylesheet", "font","image"]
+                            or "api/graphql" in route.request.url
+                            # or "/ajax/bz" in route.request.url
+                     )
+                     else route.continue_()
+                   )
+                )  
             print(f"New page: {time.perf_counter() - bros_page:.2f}s")
             pag_goto = time.perf_counter()
             page.on("request", lambda request: log_request(request, urls,state))
